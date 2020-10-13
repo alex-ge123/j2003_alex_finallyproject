d7be685ad83ad48c7ae60c1b6fd799ab8caea826
alex-ge123 <1285885272@qq.com>
2020-10-13 17:47:50 +0800

    subject生产者

---
+++ j2003_alex_coursemanagement/src/main/java/com/qf/j2003/pojo/Subject.java
@@ -0,0 +1,20 @@
+package com.qf.j2003.pojo;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+import java.util.Date;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Subject {
+    private  int sub_id;
+    private String sub_miaoshu;
+    private int deletestatus;
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
+    private Date livetime;
+
+}
--- j2003_alex_eurekaserver1/src/main/java/com/qf/j2003/config/WebSercuityConfig.java
+++ j2003_alex_eurekaserver1/src/main/java/com/qf/j2003/config/WebSercuityConfig.java
@@ -1,8 +1,11 @@
 package com.qf.j2003.config;
 
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 @EnableWebSecurity
 public class WebSercuityConfig extends WebSecurityConfigurerAdapter {
 
@@ -12,5 +15,15 @@ public class WebSercuityConfig extends WebSecurityConfigurerAdapter {
         //忽略掉eureka注册，不然会注册不进去
         httpSecurity.csrf().ignoringAntMatchers("/eureka/**");
         super.configure(httpSecurity);
+        httpSecurity.rememberMe();
+
+
     }
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        super.configure(auth);
+//        auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder())
+//                .withUser("root").password(new BCryptPasswordEncoder().encode("root")).roles();
+//    }
+
 }
