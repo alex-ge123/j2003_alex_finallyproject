5820305dee895b3a16baec415c6136d7d4ff8978
alex-ge123 <1285885272@qq.com>
2020-10-15 21:32:02 +0800

    工具类

---
+++ j2003_alex_coursemanagement/src/main/java/com/qf/j2003/config/RedisConfig.java
@@ -0,0 +1,10 @@
+package com.qf.j2003.config;
+
+import com.sun.xml.internal.ws.server.SingletonResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedisConfig {
+
+}
